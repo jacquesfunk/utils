faa56640419ddd5685a65d3c1a8bd8683d96b387
jacquesfunk <mahmad@ryanrts.com>
2024-09-19 16:42:02 -0500

    Updates

--- convert_comma_list.py
+++ convert_comma_list.py
@@ -1,7 +1,57 @@
 import subprocess
 
 # Input list of strings with line breaks
-input_string = """Exceptions_20240915.csv Exceptions_20240309.csv Exceptions_20240307.csv Exceptions_20240305.csv Exceptions_20240707.csv Exceptions_20240630.csv Exceptions_20240627.csv Exceptions_20240908.csv Exceptions_20240910.csv Exceptions_20240913.csv
+input_string = """
+a0N1H00001EBU9bUAH
+a0N1H00001EUu6jUAD
+a0N3x00000pnTIZEA2
+a0N3x00000pnTIZEA2
+a0N3x00000patEnEAI
+a0N3x00000pbQvzEAE
+a0N3x00000pnTIZEA2
+a0N3x00000pcFLpEAM
+a0N3x00000qtYAcEAM
+a0N3x00000qtwwYEAQ
+a0N3x00000pbQvzEAE
+a0N3x00000quqefEAA
+a0N3x00000qvUvOEAU
+a0N3x00000tNmZVEA0
+a0N3x00000qtYAcEAM
+a0N3x00000tOMnQEAW
+a0N3x00000qtwwYEAQ
+a0N3x00000tOm5eEAC
+a0N3x00000tOhHfEAK
+a0N3x00000tOm5eEAC
+a0N3x00000leecuEAA
+a0N3x00000sF9SjEAK
+a0N3x00000qtYAcEAM
+a0N3x00000pbQvzEAE
+a0N3x00000tOm5eEAC
+a0N3x00000leecuEAA
+a0N3x00000sF9SjEAK
+a0N3x00000patEnEAI
+a0N3x00000pnTIZEA2
+a0N3x00000pcFLpEAM
+a0N3x00000qvUvOEAU
+a0N3x00000tNmZVEA0
+a0N3x00000qtwwYEAQ
+a0N3x00000v09mSEAQ
+a0N3x00000v09mSEAQ
+a0N3x00000sF9SjEAK
+a0N3x00000vLeh8EAC
+a0N3x00000vUlwjEAC
+a0N3x00000vUu7bEAC
+a0N3x00000vW4VXEA0
+a0N3x00000vLeh8EAC
+a0N3x00000tFAH3EAO
+a0N3x00000vV6n6EAC
+a0N3x00000vLeh8EAC
+a0N3x00000vLeh8EAC
+a0N3x00000qv4vaEAA
+a0N1H00001EBU9bUAH
+a0N1H00001EUu6jUAD
+a0NVu000001cmSrMAI
+a0NVu0000022Im5MAE
 """
 
 def copy2clip(txt):
@@ -13,8 +63,7 @@ def copy2clip(txt):
 # result = ", ".join(strings_list)
 
 # Split the input into a list of strings with quotes around each item
-# strings_list = input_string.strip().split("\n")
-strings_list = input_string.strip().split(" ")
+strings_list = input_string.strip().split("\n")
 formatted_list = [f"'{name}'" for name in strings_list]
 result = ", ".join(formatted_list)
 
--- count_column_vals.py
+++ count_column_vals.py
@@ -1,62 +1,50 @@
 import polars as pl
 from rich import print
 
+
 def load_data(file_path):
     return pl.read_csv(file_path)
+
 
 def count_total_rows(df):
     return len(df)
 
-def count_exit_in_column(df):
-    exit_counts = {
-        column: df.select(
-            pl.col(column).cast(pl.Utf8).str.to_lowercase().str.count_matches('exit', literal=True)
-        ).sum().item()
-        for column in df.columns
-    }
-    return exit_counts
 
-def sort_columns_by_occurrences(counts):
-    return sorted(counts, key=counts.get, reverse=True)
+def sort_columns_by_non_null(df: pl.DataFrame) -> list:
+    # Count non-null values for each column
+    non_null_counts = df.select(
+        [pl.col(col).drop_nulls().count().alias(col) for col in df.columns]
+    )
 
-def calculate_differences(counts1, counts2, columns):
-    return {column: counts2.get(column, 0) - counts1.get(column, 0) for column in columns}
+    # Convert to dictionary and filter columns with non-null values greater than 0
+    sorted_columns = dict(
+        zip(non_null_counts.columns, non_null_counts.row(0))
+    )  # Convert to dictionary
+    sorted_columns = {k: v for k, v in sorted_columns.items() if v > 0}
 
-def print_results(total_rows, sorted_columns, counts, title):
+    # Sort columns by non-null counts in descending order
+    sorted_columns = sorted(sorted_columns.items(), key=lambda x: x[1], reverse=True)
+
+    return sorted_columns
+
+
+def print_results(total_rows, sorted_columns, title):
     print(f"Total rows: {total_rows}")
     print(f"{title}:")
-    for column in sorted_columns:
-        print(f"{column}: {counts[column]}")
-    print()
+    for col, count in sorted_columns:
+        print(f"{col}: {count}")
 
-def print_differences(differences, sorted_columns_diff):
-    print("Difference in counts in old vs new dataset:")
-    for column in sorted_columns_diff:
-        print(f"{column}: {differences[column]}")
 
 # Load datasets
-df1 = load_data('cadence_20240503_182156000.csv')
-df2 = load_data('CadenceSalesforceActiveInactiveCriteria.csv')
+df = load_data(
+    r"C:\Users\mahmad\OneDrive - Ryan RTS\Downloads\dim_fuelcard_202409191058.csv"
+)
 
 # Count total rows in each DataFrame
-total_rows_df1 = count_total_rows(df1)
-total_rows_df2 = count_total_rows(df2)
+total_rows_df = count_total_rows(df)
 
-# Count occurrences of 'exit' in each column
-count_df1 = count_exit_in_column(df1)
-count_df2 = count_exit_in_column(df2)
-
-# Sort columns by the number of occurrences of 'exit'
-sorted_df1 = sort_columns_by_occurrences(count_df1)
-sorted_df2 = sort_columns_by_occurrences(count_df2)
-
-# Calculate differences in counts between old and new datasets
-differences = calculate_differences(count_df1, count_df2, df1.columns)
-
-# Sort columns by the differences in counts
-sorted_columns_diff = sorted(df1.columns, key=lambda column: differences[column], reverse=False)
+# Sort columns by the number of non-null values
+sorted_columns = sort_columns_by_non_null(df)
 
 # Print results
-print_results(total_rows_df1, sorted_df1, count_df1, "Sorted column order by number of 'exit' occurrences in old dataset")
-print_results(total_rows_df2, sorted_df2, count_df2, "Sorted column order by number of 'exit' occurrences in new dataset")
-print_differences(differences, sorted_columns_diff)
+print_results(total_rows_df, sorted_columns, "Counts")
